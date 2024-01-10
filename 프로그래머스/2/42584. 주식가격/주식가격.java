191fc4b7d6263ed70ce544e183308a5ed4cf69f4
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-01-10 21:02:18 +0900

    [level 2] Title: 주식가격, Time: 24.66 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,14 +1,29 @@
-class Solution {
-    public int[] solution(int[] prices) {
-        /* 현재 시점의 가격을 유지한 기간. */
+import java.util.LinkedList;
+import java.util.Queue;
+ 
+public class Solution {
+    public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for(int i = 0; i < prices.length; i++){
-            for(int j = i + 1; j < prices.length; j++){
-                answer[i]++;
-                if (prices[i] > prices[j]) break;
-            }
+        Queue<Integer> queue = new LinkedList<>();
+ 
+        for (int price : prices) {
+            queue.add(price);
         }
-        
+ 
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int curPrice = queue.poll();
+            for (int i = (prices.length - queue.size()); i < prices.length; i++) {
+                if (curPrice > prices[i]) {
+                    answer[index]++;
+                    break;
+                }
+                if (curPrice <= prices[i]) {
+                    answer[index]++;
+                }
+            }
+            index++;
+        }
         return answer;
     }
 }
