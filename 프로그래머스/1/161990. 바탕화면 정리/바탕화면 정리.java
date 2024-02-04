23607573875d2d6746241bb8a54aa216b739ef73
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-02-04 20:10:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -6,27 +6,20 @@
 */
 class Solution {
     static int[] solution(String[] wallpaper) {
-        // x -> 행, y -> 열
-        int[] answer = new int[4];
-        
-        // 가장 왼쪽의 좌표점 S(lux, luy)
-        answer[0] = wallpaper.length + 1; // 행의 최대값보다 큰 값으로 초기화
-        answer[1] = wallpaper[0].length() + 1; // 열의 최대값보다 큰 값으로 초기화
-
-        // 가장 오른쪽의 좌표점 E(rdx, rdy)
-        answer[2] = -1; // 행의 최소값(0) 보다 작은 값으로 초기화
-        answer[3] =  -1; // 열의 최소값(0) 보다 작은 값으로 초기화
+        // top, left, bottom, right 초기화
+        int[] answer =  {wallpaper.length, wallpaper[0].length(), -1, -1};
         
         for (int i = 0; i < wallpaper.length; i++) {
             if (wallpaper[i].contains("#")) {
-                // lux, rdx -> #이 존재하는 배열 index중 최소값과 최대값
+                // lux -> #이 존재하는 배열 index중 가장 작은 index
                 answer[0] = Math.min(answer[0], i);
+                // rdx -> #이 존재하는 배열 index중 가장 큰 index
                 answer[2] = Math.max(answer[2], i+1);
 
-                // luy -> 배열내 문자열의 각 첫번째 # index 중 최소값
+                // luy -> 배열내 각 문자열에서 #의 첫번째 index 
                 int minColumnIndex = wallpaper[i].indexOf("#");
                 answer[1] = Math.min(answer[1], minColumnIndex);
-                // rdy -> 배열내 문자열의 각 마지막 # index 중 최대값
+                // rdy -> 배열내 각 문자열에서 #의 마지막 index 
                 int maxColumnIndex = wallpaper[i].lastIndexOf("#");
                 answer[3] = Math.max(answer[3], maxColumnIndex+1);
             }
