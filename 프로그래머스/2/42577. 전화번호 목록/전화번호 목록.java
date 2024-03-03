5d866914a189bb6153b401fd5fb9aadd3d574e59
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-03-03 21:49:34 +0900

    [level 2] Title: 전화번호 목록, Time: 278.23 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,24 +1,17 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        HashMap<String, Boolean> map = new HashMap<>();
-         // 전화번호를 해시로 매핑하여 해시맵에 저장
-        for (String phoneNumber : phone_book) {
-            map.put(phoneNumber, true);
-        }
+        boolean answer = true;
+        Arrays.sort(phone_book);
         
-        // 각 전화번호의 접두사가 해시 테이블에 있는지 확인
-        for (String phoneNumber : phone_book) {
-            for (int i = 1; i < phoneNumber.length(); i++) {
-                String prefix = phoneNumber.substring(0, i);
-                if (map.containsKey(prefix)) {
-                    return false; // 접두사를 발견한 경우 false를 반환
-                }
+        for(int i = 0; i < phone_book.length - 1; i++){
+            if(phone_book[i + 1].startsWith(phone_book[i])){
+                answer = false;
+                break;
             }
         }
         
-        return true;
+        return answer;
     }
 }
