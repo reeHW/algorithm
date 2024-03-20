7b774404af307636e2a7029311d7dda173c64327
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-03-20 22:12:57 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        // 전체 격자의 수
+        int total = brown + yellow;
+        
+        // 카펫의 세로 길이 후보를 탐색
+        for (int height = 3; height <= total / 2; height++) {
+            // 세로 길이로 나누어 떨어지는 경우에만 확인
+            if (total % height == 0) {
+                int width = total / height;
+                
+                // yellow 격자수 check
+                if ((width - 2) * (height - 2) == yellow) {
+                    answer[0] = width;
+                    answer[1] = height;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
