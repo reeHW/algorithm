783d2abcdaeffa476e5abbce581dac8c9ecbb21c
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-03-21 00:10:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -6,8 +6,7 @@ class Solution {
         int total = brown + yellow;
         
         /* 카펫의 세로 길이 후보를 탐색 
-           1. 갈색 타일은 노란색 타일을 감싸야 함. -> brown = yellow + 2
-           2. 가로 길이 >= 세로 길이 -> 가로 길이는 전체 격자의 절반보다 커질 수 없음
+            yellow보다 brown이 
         */
         for (int height = 3; height <= total / 2; height++) {
             // 세로 길이로 나누어 떨어지는 경우에만 확인
