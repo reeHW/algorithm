0a6925c3bf57e26d20ed0d51333015255c65466f
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-04-17 07:02:37 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.38 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
         int successCount = 0;
-        int maxHealth = health;
+        int currentHealth = health; // 현재 체력 변수 추가
         // 공격 기록을 맵으로 기록
         HashMap<Integer, Integer> attackLog = new HashMap<>();
         for (int[] attack : attacks) {
@@ -15,24 +15,23 @@ class Solution {
             // 공격 시간일 경우
             if (attackLog.containsKey(time)) {
                 successCount = 0;
-                health -= attackLog.get(time);
-                if (health <= 0) {
+                currentHealth -= attackLog.get(time); // 현재 체력에서 공격 피해를 받음
+                if (currentHealth <= 0) {
                     return -1;
                 }
             } else { // 공격 시간이 아닐 경우
                 successCount++;
                 if (successCount < bandage[0]) {
-                    health += bandage[1];
+                    currentHealth += bandage[1]; // 붕대 회복량 적용
                 } else if (successCount == bandage[0]) {
-                    health += bandage[1];
-                    health += bandage[2];
+                    currentHealth += bandage[1] + bandage[2]; // 추가 회복량 적용
                     successCount = 0;
                 }
-                if (health > maxHealth) {
-                    health = maxHealth;
+                if (currentHealth > health) {
+                    currentHealth = health;
                 }
             }
         }
-        return health;
+        return currentHealth;
     }
 }
