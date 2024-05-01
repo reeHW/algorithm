95516c668436a0e95e7fd30395586eb39be44ea8
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-05-02 01:42:23 +0900

    [level 2] Title: 요격 시스템, Time: 0.66 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -13,12 +13,12 @@ public class Solution {
         for (int[] target : targets) {
             int start = target[0];
             int end = target[1];
-            // 현재 폭격 미사일의 시작점이 이전에 요격한 가장 높은 끝점보다 크거나 같은 경우
+            // 현재 폭격 미사일의 시작점이 이전에 요격한 가장 오른쪽 끝점과 같거나 더 앞서 있는 경우
             if (start >= currentEnd) {
                 count++;
                 currentEnd = end;
             } else {
-                // 현재 폭격 미사일의 시작점이 이전에 요격한 가장 높은 끝점보다 작은 경우
+                // 현재 폭격 미사일의 시작점이 이전에 요격한 가장 높은 끝점보다 좌표상 왼쪽에 있는 경우
                 // 해당 미사일이 이전에 요격한 미사일에 포함되므로 끝점을 갱신
                 currentEnd = Math.min(currentEnd, end);
             }
