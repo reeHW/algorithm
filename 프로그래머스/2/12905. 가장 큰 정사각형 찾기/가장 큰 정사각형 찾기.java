f2e26d886ec03fb92534c9a7b0e9c1b45bc6f92d
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-05-15 23:49:38 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 14.28 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(int[][] board) {
-        int maxSquareSize = 0; // 최대 정사각형의 한 변의 길이
+        int maxVal = 0; // 최대 정사각형의 한 변의 길이
         int row = board.length;
         int col = board[0].length;
+        
+        if (row == 1 && col == 1) {
+            if (board[0][0] == 1) return 1;
+        }
 
-        // 후보 셀을 순회하면서 최대 정사각형의 한 변의 길이 계산
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
                 if (board[i][j] == 1) {
-                    if (i > 0 && j > 0) {
-                        board[i][j] = Math.min(Math.min(board[i-1][j], board[i][j-1]), board[i-1][j-1]) + 1;
-                    }
-                    maxSquareSize = Math.max(maxSquareSize, board[i][j]);
+                    board[i][j] = Math.min(board[i-1][j-1], Math.min(board[i-1][j], board[i][j-1])) + 1;
+                    maxVal = Math.max(maxVal, board[i][j]);
                 }
             }
         }
 
-        // 최대 정사각형의 넓이 반환
-        return maxSquareSize * maxSquareSize;
+        return maxVal * maxVal;
     }
 }
