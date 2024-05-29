c542c2ad38184d1d53f518f0caeadca518f14242
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-05-29 21:37:28 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.05 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,24 +1,17 @@
 /*
 0번째 1
-1번째 11011
-2번째 11011 / 11011 / 00000 / 11011 / 11011 
-length : 25 segmentEnd : 11 segmentEnd : 15   (25/5)*2+1
-
-3번째 11011 11011 00000 11011 11011 / 11011 11011 00000 11011 11011 / 00000  00000  00000  00000  00000 / 11011 11011 00000 11011 11011 / 11011 11011 00000 11011 11011
-length 125 segmentEnd : 51 segmentEnd : 80    (125/5)*2+1 
+n=1 11011
+n=2 11011 / 11011 / 00000 / 11011 / 11011 
+n=3 11011 11011 00000 11011 11011 / 11011 11011 00000 11011 11011 / 00000  00000  00000  00000 00000 / 11011 11011 00000 11011 11011 / 11011 11011 00000 11011 11011
 
 -> n번째 비트열의 길이는: 5^n
 
 n=1 11011
 n=2 4/4/0/4/4  
 n=3 16/16/0/16/16
-n=4 64/64/0/64/64
-...
+```
 -> n번째 비트열의 1의 개수는 4^(n-1)
 
-n=3
-l=10, r = 18  
--> 16/16/0/16/16
 */
 class Solution {
     public int solution(int n, long l, long r) {
@@ -38,33 +31,22 @@ class Solution {
 
         int totalOnes = 0;
         for (int i = 0; i < 5; i++) {
+            if (i == 2) continue;
             
             long segmentStart = start + i * length;
             long segmentEnd = segmentStart + length - 1;
-          
-            System.out.println("segmentStart: " + segmentStart);
-            System.out.println("segmentEnd: " + segmentEnd);
-            //System.out.println("length: " + length);
-            //System.out.println("length/5 + 1: " + length/5 + 1);
-
+            
             if (segmentEnd < l || segmentStart > r) {
-                System.out.println("Skipped");
                 continue;
             }
 
             if (l <= segmentStart && segmentEnd <= r) {
-                System.out.println("Entirely Inside");
                 totalOnes += bitOnesCount[i];
             } else {
-                System.out.println("Partially Inside");
-                 if (i == 2) {
-                    System.out.println("Skipping middle segment");
-                    continue;
-                }
                 totalOnes += countOnes(n - 1, segmentStart, segmentEnd, l, r);
             }
         }
-
+        
         return totalOnes;
     }
 }
