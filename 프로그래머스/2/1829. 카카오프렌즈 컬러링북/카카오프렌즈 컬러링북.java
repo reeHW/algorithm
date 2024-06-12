2c2a9d16f607bbf059fb31a6f5cb94ff68f9ddd4
HYEONE <68371436+reeHW@users.noreply.github.com>
2024-06-12 21:22:11 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 13.91 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -1,18 +1,18 @@
 class Solution {
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 
     public int[] solution(int m, int n, int[][] picture) {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
 
         boolean[][] visited = new boolean[m][n];
-        int[] dx = {-1, 1, 0, 0};  // 상하 이동
-        int[] dy = {0, 0, -1, 1};  // 좌우 이동
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (picture[i][j] > 0 && !visited[i][j]) {
                     numberOfArea++;
-                    int areaSize = dfs(picture, visited, i, j, picture[i][j], m, n, dx, dy);
+                    int areaSize = dfs(picture, visited, i, j, picture[i][j], m, n);
                     maxSizeOfOneArea = Math.max(maxSizeOfOneArea, areaSize);
                 }
             }
@@ -21,16 +21,24 @@ class Solution {
         return new int[]{numberOfArea, maxSizeOfOneArea};
     }
 
-    private int dfs(int[][] picture, boolean[][] visited, int x, int y, int color, int m, int n, int[] dx, int[] dy) {
-        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || picture[x][y] != color) {
-            return 0;
-        }
-
+    private int dfs(int[][] picture, boolean[][] visited, int x, int y, int color, int m, int n) {
         visited[x][y] = true;
         int areaSize = 1;
 
         for (int i = 0; i < 4; i++) {
-            areaSize += dfs(picture, visited, x + dx[i], y + dy[i], color, m, n, dx, dy);
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            // 범위를 벗어남
+            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+
+            // 방문한 셀
+            if (visited[nx][ny]) continue;
+            
+            // 같은 색이 아님
+            if(picture[nx][ny] != color) continue;
+            
+            areaSize += dfs(picture, visited, nx, ny, color, m, n);
         }
 
         return areaSize;
